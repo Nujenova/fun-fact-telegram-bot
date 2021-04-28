de4042d4e1d53eb8eb56a7478e0bfb89fc6e087d
yini ong <nujenova@gmail.com>
2021-04-28 10:34:18 +0800

    update main

--- main.py
+++ main.py
@@ -1,44 +1,86 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import os
-from dotenv import load_dotenv
-import responses as r
+import key
+import arrow
+import requests
+import json
+import string
 
-load_dotenv()
+## Import your API key ##
 
-key = os.getenv('token')
+key = key.secret_token
 
-print("bot is starting...")
+## Print something to show that the bot is alive ##
+
+print("bot is running....")
+
+## Utility Functions to be used in the Command Functions ## 
+## get creative! you can do whatever you want here ##
+
+def response(input):
+    s0 = input.lower()
+    s1 = "".join([i for i in s0 if i not in string.punctuation])
+    text = s1.split()
+    
+    hello_ls = ["hello", "hi", "hey", "oi"]
+    check =  any(word in text for word in hello_ls)
+    
+    if check is True:
+        return "Hello!"
+
+    if input in ("who are you?"):
+        return "i am bot!"
+    
+    if input in ("how are you?"):
+        return "im fine, thank you. how about you?"
+
+    return "I dont understand your question"
+
+def report_time():
+    utc = arrow.utcnow()
+    date_time = utc.shift(hours=8)
+    date_time = date_time.format('YYYY-MM-DD HH:mm')
+
+    return "the time now is {}".format(date_time)
+
+def random_fact():
+## https://uselessfacts.jsph.pl/
+  response = requests.get("https://uselessfacts.jsph.pl/random.json?language=en")
+  json_data = json.loads(response.text)
+  data = json_data['text']
+  
+  return data
+
+## Create Command functions ##
+
+## update is what the bot will use to respond to user inputs
+## context is a slightly more advanced concept where it can take a user's metadata and determine if certain functions should be executed for certain users or not. (switching functions on and off)
 
 def start_command(update, context):
     update.message.reply_text('type something to begin')
 
-def help_command(update, context):
-    update.message.reply_text('need help? google')
+def time_command(update, context):
+    update.message.reply_text(report_time())
 
 def fact_command(update,context):
-    update.message.reply_text(r.random_fact())
+    update.message.reply_text(random_fact())
 
-def handle_msg(update, context):
-    text = str(update.message.text).lower()
-    responses = r.sample_responses(text)
+def reply_msg(update, context):
+    input = str(update.message.text)
+    responses = response(input)
 
     update.message.reply_text(responses)
 
-def error(update, context):
-    print(f"Update {update} caused error {context.error}")
-
+## Define main function to run the script ##
 
 def main():
     updater = Updater(key, use_context = True)
-    dp = updater.dispatcher
+    dp = updater.dispatcher  ## receives user inputs, and dispatches it to the various handlers
 
     dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("time", time_command))
     dp.add_handler(CommandHandler("funfact",fact_command))
 
-    dp.add_handler(MessageHandler(Filters.text, handle_msg))
-
-    dp.add_error_handler(error)
+    dp.add_handler(MessageHandler(Filters.text, reply_msg))
 
     updater.start_polling() 
     updater.idle()
